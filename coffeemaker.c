74e687dfc0f126d88c93f5a18a0cd1dd801d50bf
jasonsandeep <jasonsandeep123@gmail.com>
2024-10-04 12:32:03 +0400

    Add files via upload

--- coffeemaker.c
+++ coffeemaker.c
@@ -4,6 +4,12 @@ void admin();
  int main()
  {
 int option;
+int a;
+int b;
+int c;
+float payment;
+float price=0.0;
+
  printf("1.Order a Coffee \n ");
  printf("2.Admin mode \n ");
  printf("3.Exit \n ");
@@ -12,8 +18,57 @@ int option;
  scanf("%d",&option);
  if (option==1)
 {
-printf("Choose coffee:");
-return 0;
+	int coffee;
+	while(1){
+printf("/-----------------------------------------------------/\n");
+printf("Choose coffee:\n");
+printf("1.Espresso - 3.5 AED \n ");
+printf("2.Cappuccino - 4.5 AED\n ");
+printf("3.Mocha - 5.5 AED \n ");
+printf("0.Exit\n ");
+printf("Enter option 0-3:");
+ 
+scanf("%d",&coffee);
+if (coffee==1){
+	price = 3.5;
+	printf("You chose Espresso - 3.5 AED\n");
+	printf("Insert 3.5 AED in exact change:",price);
+	scanf("%f",&payment);
+	
+	if (payment==price)
+	{
+	printf("Enjoy your Coffee!");
+	break;
+	}
+	
+	
+else if (coffee==2)
+{
+	
+	printf("You chose Cappuccino - 4.5 AED");
+}
+else if (coffee==3)
+{
+	printf("You chose Mocha - 5.5 AED");	
+}
+else if (coffee==0)
+{
+	break;
+}
+
+
+else
+{
+	printf("Invalid Option. Try Again.\n");
+	continue;
+}
+
+	
+
+
+
+}
+}
 }
 else if (option==2)
 {
@@ -31,5 +86,6 @@ else
 	printf("Invalid Option. Try Again.\n");
 }
 }
+
 
  
